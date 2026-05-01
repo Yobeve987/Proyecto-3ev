ae5352a4a156ea65aec2ada53fb7cca61b34c9f7
yobeve <yobeve@alumnatflorida.es>
2026-05-01 15:46:29 +0200

    primer commit

---
+++ query.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE proyecto;
+USE proyecto;
+CREATE TABLE productos (
+    id INT AUTO_INCREMENT PRIMARY KEY, --
+    nombre VARCHAR(100), --
+    precio DECIMAL(10,2), --
+    stock INT), --
+    marca VARCHAR (50), --
+    modelo VARCHAR(50) NULL, --
+    garantia VARCHAR(5), --
+    talla VARCHAR(30), --
+    color VARCHAR(30), --
+    material VARCHAR(30), --
+    genero VARCHAR(30),
+    descripcion VARCHAR(200) --
+);  
